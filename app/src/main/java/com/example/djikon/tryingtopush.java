d3cbae940d4bf784abcd0771baee501bb940f96a
Hamza-Ali <hamzaregardless333@gmail.com>
2020-07-10 21:54:16 -0700

    tryingtopush

--- app/src/main/java/com/example/djikon/tryingtopush.java
+++ app/src/main/java/com/example/djikon/tryingtopush.java
@@ -7,4 +7,13 @@ public class tryingtopush {
 
     //this is our first class where i m taking you with me a
 
+    private void tryingfunction () {
+         int i = 0;
+         int result = 0;
+         result=i+result;
+         if (result==0) {
+             result ==90;
+             return;
+         }
+    }
 }
